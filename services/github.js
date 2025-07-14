00162ec80ec5b1f7d1c8627a30c9a856cbfea2e2
Kinsu Rajput <kissuxrajput@gmail.com>
2025-07-15 04:02:06 +0530

    Update github.js

--- services/github.js
+++ services/github.js
@@ -1,6 +1,13 @@
-import axios from 'axios';
+const GITHUB_API = 'https://api.github.com';
 
-export const fetchUserRepos = async (username) => {
-  const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=100`);
-  return res.data;
-};
+export async function fetchUserProfile(username) {
+  const res = await fetch(`${GITHUB_API}/users/${username}`);
+  if (!res.ok) throw new Error('Failed to fetch profile');
+  return await res.json();
+}
+
+export async function fetchUserRepos(username) {
+  const res = await fetch(`${GITHUB_API}/users/${username}/repos?per_page=100&sort=updated`);
+  if (!res.ok) throw new Error('Failed to fetch repos');
+  return await res.json();
+}
